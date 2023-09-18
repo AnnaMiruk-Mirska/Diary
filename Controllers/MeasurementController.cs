0c07809c756b4c2f052cdb2b12eb05b3795e1ab3
AnnaMiruk-Mirska <annamir019@student.polsl.pl>
2023-09-18 22:35:09 +0200

    added DatePicker to add measurement view

--- Controllers/MeasurementController.cs
+++ Controllers/MeasurementController.cs
@@ -19,6 +19,8 @@ namespace Diary.Controllers
         IFirebaseClient client;
 
 
+
+
         public MeasurementController()
         {
             IFirebaseConfig config = new FirebaseConfig
--- Models/Measurement.cs
+++ Models/Measurement.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +10,22 @@ namespace Diary.Models
     public class Measurement
     {
         public string Id { get; set; }
+
+        [DisplayName("Data")]
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
+
+
+        [DisplayName("Ciśnienie rozkurczowe")]
+        [DataType(DataType.CreditCard)]
         public int Systolic { get; set; }
+
+        [DisplayName("Ciśnienie skurczowe")]
+        [DataType(DataType.CreditCard)]
         public int Diastolic { get; set; }
+
+        [DisplayName("Puls")]
+        [DataType(DataType.CreditCard)]
 
         public int HeartRate { get; set; }
     }
